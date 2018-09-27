a911d644f5f58e7a5df3e4337c6895ec71b597b4
Hbuz <m.basilico10@gmail.com>
2018-09-28 01:06:54 +0200

    Finished

--- client/src/components/tickets/Tickets.jsx
+++ client/src/components/tickets/Tickets.jsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
 import TableCell from '@material-ui/core/TableCell'
 import TableRow from '@material-ui/core/TableRow'
-import ticketPic from '../../lib/images/football_ticket'
 
 export default function Tickets(props) {
-
+  const splitted = props.picture.split('\\')
+  const ticketPic = require(`../../lib/images/${splitted[splitted.length -1]}`)
   return (
     <TableRow key={props.id}>
       <TableCell component="th" scope="row"><img src={ticketPic} alt="No image for this ticket" /></TableCell>
